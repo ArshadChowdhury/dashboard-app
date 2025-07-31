b5629a9ccbc4a4a2917eba2d11e7df9f113e54ea
Arshad <arshadchowdhury46@gmail.com>
2025-08-01 02:32:36 +0600

    chore: installed zustand & setup the cart store for get, add items to cart

---
+++ app/store/useCartStore.ts
@@ -0,0 +1,69 @@
+import { create } from "zustand";
+
+export interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  // Add any other product fields you need
+}
+
+interface CartState {
+  cart: CartItem[];
+  addToCart: (product: Omit<CartItem, "quantity">) => void;
+  removeFromCart: (productId: string) => void;
+  updateQuantity: (productId: string, quantity: number) => void;
+  getCartTotal: () => number;
+  getCartItemCount: () => number;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  cart: [],
+
+  addToCart: (product) => {
+    const { cart } = get();
+    const existingItem = cart.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      set({
+        cart: cart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      });
+    } else {
+      set({
+        cart: [...cart, { ...product, quantity: 1 }],
+      });
+    }
+  },
+
+  removeFromCart: (productId) => {
+    set((state) => ({
+      cart: state.cart.filter((item) => item.id !== productId),
+    }));
+  },
+
+  updateQuantity: (productId, quantity) => {
+    if (quantity <= 0) {
+      get().removeFromCart(productId);
+    } else {
+      set((state) => ({
+        cart: state.cart.map((item) =>
+          item.id === productId ? { ...item, quantity } : item
+        ),
+      }));
+    }
+  },
+
+  getCartTotal: () => {
+    const { cart } = get();
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  },
+
+  getCartItemCount: () => {
+    const { cart } = get();
+    return cart.reduce((total, item) => total + item.quantity, 0);
+  },
+}));
